eb94c5e848cccb0eb74ad3b7b91f30fa17451ea6
Developer <you@example.com>
2025-07-15 23:20:35 -0700

    3

--- js/extns_page.js
+++ js/extns_page.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Cargar header
-  fetch('../../header.html')
+  fetch('pag_ext_bs/header.html')
     .then(res => res.text())
     .then(data => {
       document.getElementById('header').innerHTML = data;
       // Aquí puedes inicializar menú, etc.
     });
 
+     fetch('../header.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+      // Aquí puedes inicializar menú, etc.
+    });
   
 });
